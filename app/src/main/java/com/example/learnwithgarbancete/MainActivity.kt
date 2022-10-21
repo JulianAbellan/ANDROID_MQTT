b043e7309bc8bc2da40797878d8a0e90448e6634
RAÚL ARROYO TORRIJOS <Raul.Arroyo1@alu.uclm.es>
2022-10-21 14:28:15 +0200

    Inicio proyecto

---
+++ app/src/main/java/com/example/learnwithgarbancete/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.learnwithgarbancete
+
+import android.support.v7.app.*
+import android.os.*
+import android.widget.*
+import android.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var maths: Button;
+    lateinit var text: TextView;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        maths = findViewById(R.id.button)
+        text = findViewById(R.id.textView)
+
+    }
+
+    fun pelao(view : View){
+        text.setText("Pelao deja de ligar cabron")
+    }
+
+    fun estudiar(view: View){
+        text.setText("Y vamos a hacer ipo ajajjaja")
+    }
+}
