09e0dcf88a1120c8d9fbe25db6d43c2a02bcada0
JULIÁN ABELLÁN ABAD <Julian.Abellan@alu.uclm.es>
2022-10-22 20:25:11 +0200

    poner valor a los botones de opciones (no va)

--- app/src/main/java/com/example/learnwithgarbancete/MathsGame.kt
+++ app/src/main/java/com/example/learnwithgarbancete/MathsGame.kt
@@ -8,12 +8,17 @@ import java.util.Random
 
 class MathsGame : AppCompatActivity() {
 
-    var operandoA : Int = 0
-    var operandoB : Int = 0
-    var operacion : Int = 0
-    var resultado : Int = 0
-    var operacionText : TextView? = null
-    lateinit var atrasMaths : Button
+    var operandoA: Int = 0
+    var operandoB: Int = 0
+    var operacion: Int = 0
+    var resultado: Int = 0
+    lateinit var lista : List<Int>
+    var operacionText: TextView? = null
+    lateinit var atrasMaths: Button
+    lateinit var buttonOpA: Button
+    lateinit var buttonOpB: Button
+    lateinit var buttonOpC: Button
+    lateinit var buttonOpD: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +27,49 @@ class MathsGame : AppCompatActivity() {
 
         operacionText = findViewById(R.id.operacionText)
         mostrarOp()
-
+        crearOpciones()
 
         atrasMaths = findViewById(R.id.atrasMaths)
+
         atrasMaths.setOnClickListener() {
-            val intent : Intent = Intent(this, MainActivity::class.java)
+            val intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
     }
 
-    fun mostrarOp () {
+    fun mostrarOp() {
         operacionText?.setText(generarOp())
     }
 
-    fun generarOp (): String {
+    fun setOperaciones(){
+        buttonOpA.setText(lista[0])
+        buttonOpB.setText(lista[1])
+        buttonOpC.setText(lista[2])
+        buttonOpD.setText(lista[3])
+    }
+
+    fun crearOpciones() {
+        var num1 = 0
+        var num2 = 0
+        var num3 = 0
+
+        do {
+            num1 = resultado + Random().nextInt(6) - 3
+            num2 = resultado + Random().nextInt(6) - 3
+            num3 = resultado + Random().nextInt(6) - 3
+        } while (!distintos(num1, num2, num3))
+
+        lista = listOf(resultado, num1, num2, num3).shuffled()
+
+        setOperaciones()
+    }
+
+    fun distintos(num1 : Int, num2 : Int, num3 : Int) : Boolean{
+       return (num1 != num2) && (num1 != num3) && (num1 != resultado) && (num2 != num3) && (num2 != resultado) && (num3 != resultado)
+    }
+
+    fun generarOp(): String {
         var operandoA = Random().nextInt(9) + 1
         var operandoB = Random().nextInt(9) + 1
         var operacion = Random().nextInt(2)
