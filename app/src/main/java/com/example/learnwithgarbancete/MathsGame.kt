8645bc1f1ea753760892a5a2dc620621804c585c
JULIÁN ABELLÁN ABAD <Julian.Abellan@alu.uclm.es>
2022-10-24 12:29:42 +0200

    acabada la funcionalidad de maths y añadimos el juego de idiomas

--- app/src/main/java/com/example/learnwithgarbancete/MathsGame.kt
+++ app/src/main/java/com/example/learnwithgarbancete/MathsGame.kt
@@ -12,8 +12,14 @@ class MathsGame : AppCompatActivity() {
     var operandoB: Int = 0
     var operacion: Int = 0
     var resultado: Int = 0
-    lateinit var lista : List<Int>
+    var scorePuntos: Int = 0
+    var i: Int = 0
+    lateinit var lista: List<Int>
+    var strOp: String? = null
     var operacionText: TextView? = null
+    var nivelText: TextView? = null
+    var score: TextView? = null
+    var resumenText: TextView? = null
     lateinit var atrasMaths: Button
     lateinit var buttonOpA: Button
     lateinit var buttonOpB: Button
@@ -27,27 +33,108 @@ class MathsGame : AppCompatActivity() {
 
 
         operacionText = findViewById(R.id.operacionText)
+        nivelText = findViewById(R.id.nivelText)
+        score = findViewById(R.id.score)
         buttonOpA = findViewById(R.id.buttonOpA)
         buttonOpB = findViewById(R.id.buttonOpB)
         buttonOpC = findViewById(R.id.buttonOpC)
         buttonOpD = findViewById(R.id.buttonOpD)
         atrasMaths = findViewById(R.id.atrasMaths)
+        resumenText = findViewById(R.id.resumenText)
 
         atrasMaths.setOnClickListener() {
             val intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
+        buttonOpA.setOnClickListener() {
+            if (resultado == lista[0]) {
+                operacionText?.setText("CORRECT")
+            } else {
+                operacionText?.setText("INCORRECT")
+            }
+            checkResults()
+        }
+
+        buttonOpB.setOnClickListener() {
+            if (resultado == lista[1]) {
+                operacionText?.setText("CORRECT")
+            } else {
+                operacionText?.setText("INCORRECT")
+            }
+            checkResults()
+        }
+
+        buttonOpC.setOnClickListener() {
+            if (resultado == lista[2]) {
+                operacionText?.setText("CORRECT")
+            } else {
+                operacionText?.setText("INCORRECT")
+            }
+            checkResults()
+        }
+
+        buttonOpD.setOnClickListener() {
+            if (resultado == lista[3]) {
+                operacionText?.setText("CORRECT")
+            } else {
+                operacionText?.setText("INCORRECT")
+            }
+            checkResults()
+        }
+
+        rutina()
+    }
+
+
+    fun rutina() {
+        i+=1
+        ponerNivel()
+        setScore()
         mostrarOp()
         crearOpciones()
         setOperaciones()
+    }
+
+    fun checkResults(){
+        if(operacionText?.getText()?.equals("CORRECT") == true) {
+            scorePuntos +=1
+        }
+
+        rutina()
+    }
+
+    fun setScore(){
+        score?.setText("SCORE: ${scorePuntos}")
+    }
+
+    fun ponerNivel(){
+        nivelText?.setText("LEVEL: ${i}/5")
+
+        if(i == 5){
+            resumen()
+        }
+    }
+
+    fun resumen() {
+        buttonOpA.setEnabled(false)
+        buttonOpB.setEnabled(false)
+        buttonOpC.setEnabled(false)
+        buttonOpD.setEnabled(false)
+
+        if (scorePuntos < 2)
+            resumenText?.setText("YOU GOT ${scorePuntos} POINTS...")
+        else if (scorePuntos < 5)
+            resumenText?.setText("WOW! YOU GOT ${scorePuntos} POINTS! YOU ALMOST GOT IT!")
+        else
+            resumenText?.setText("CONGRATS! YOU GOT ${scorePuntos} POINTS! YOU ARE A REAL MATHEMATICIAN")
     }
 
     fun mostrarOp() {
         operacionText?.setText(generarOp())
     }
 
-    fun setOperaciones(){
+    fun setOperaciones() {
         buttonOpA.setText("" + lista[0])
         buttonOpB.setText("" + lista[1])
         buttonOpC.setText("" + lista[2])
@@ -69,8 +156,8 @@ class MathsGame : AppCompatActivity() {
 
     }
 
-    fun distintos(num1 : Int, num2 : Int, num3 : Int) : Boolean{
-       return (num1 != num2) && (num1 != num3) && (num1 != resultado) && (num2 != num3) && (num2 != resultado) && (num3 != resultado)
+    fun distintos(num1: Int, num2: Int, num3: Int): Boolean {
+        return (num1 != num2) && (num1 != num3) && (num1 != resultado) && (num2 != num3) && (num2 != resultado) && (num3 != resultado)
     }
 
     fun generarOp(): String {
@@ -87,11 +174,13 @@ class MathsGame : AppCompatActivity() {
         when (operacion) {
             0 -> {
                 resultado = operandoA + operandoB
-                return "$operandoA + $operandoB = ?"
+                strOp  ="$operandoA + $operandoB = ?"
+                return strOp as String
             }
             1 -> {
                 resultado = operandoA - operandoB
-                return "$operandoA - $operandoB = ?"
+                strOp = "$operandoA - $operandoB = ?"
+                return strOp as String
             }
             else -> throw Exception("Ha habido un error en la operacion. No es 0 ó 1")
         }
