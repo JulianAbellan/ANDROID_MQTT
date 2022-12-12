0e77aeac98e42cd62aa0b152b979e692e97360c9
RAÚL ARROYO TORRIJOS <Raul.Arroyo1@alu.uclm.es>
2022-12-12 15:28:16 +0100

    Healthy y strings in progresss

--- app/src/main/java/com/example/learnwithgarbancete/HealthyUnhealthy.kt
+++ app/src/main/java/com/example/learnwithgarbancete/HealthyUnhealthy.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.ColorMatrixColorFilter
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import java.util.*
 
 class HealthyUnhealthy : AppCompatActivity() {
@@ -18,17 +23,32 @@ class HealthyUnhealthy : AppCompatActivity() {
     lateinit var flechaverde: ImageView
     lateinit var fotocentro: ImageView
     lateinit var garbancete: Button
-
+    lateinit var HealthyText : TextView
+    lateinit var textGarbancete : TextView
+    lateinit var NumberScore : TextView
+    lateinit var list: kotlin.collections.List<Int>
+    var hamburguesa = R.drawable.hamburguesa
+    var lechuga = R.drawable.lechuga
+    var pizza = R.drawable.pizza
+    var manzana = R.drawable.manzana
+    var zanahoria = R.drawable.zanahoria
+    var count: Int = 0
     lateinit var texttospeech: TextToSpeech
     lateinit var settings : SharedPreferences
     lateinit var tipodaltonico : String
-
+    lateinit var sensorManager: SensorManager
+    lateinit var sensor: Sensor
+    lateinit var sensorEventListener: SensorEventListener
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_healthy_unhealthy)
         settings = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        count = 0
+        list = listOf<Int>(zanahoria,hamburguesa,lechuga,pizza,manzana)
 
         texttospeech = TextToSpeech(this, TextToSpeech.OnInitListener {
 
@@ -41,7 +61,6 @@ class HealthyUnhealthy : AppCompatActivity() {
                 val locSpanish = Locale("spa", "ES")
                 texttospeech.setLanguage(locSpanish)
             }
-
         })
 
         backButton2 = findViewById(R.id.backButton3)
@@ -49,6 +68,9 @@ class HealthyUnhealthy : AppCompatActivity() {
         flechaverde = findViewById(R.id.flechaverde)
         garbancete = findViewById(R.id.garbancete)
         fotocentro = findViewById(R.id.imageView)
+        HealthyText = findViewById(R.id.FoodDescription)
+        NumberScore = findViewById(R.id.NumberScore)
+        textGarbancete = findViewById(R.id.textGarbancete)
 
         tipodaltonico = settings.getString("daltonico", "").toString()
         aplicarTipoDaltonismo(tipodaltonico)
@@ -60,6 +82,114 @@ class HealthyUnhealthy : AppCompatActivity() {
             startActivity(intent)
         }
 
+        sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(sensorEvent: SensorEvent) {
+                val x = sensorEvent.values[0]
+                if (x < -5) {
+                    onPause()
+                    giroDerecha()
+                } else if (x > 5) {
+                    onPause()
+                    giroIzq()
+                }
+            }
+            override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
+        }
+        if (sensor == null) finish()
+
+    }
+
+    fun giroDerecha(){
+        if(count != list.size) {
+            var aux = list.get(count)
+            NumberScore.setText("GiroDer " + count)
+
+            if(aux == zanahoria){
+                count++
+                NumberScore.setText("Zanah " + count)
+
+            }
+
+            if(aux == lechuga || aux == manzana || aux == zanahoria) {
+                textGarbancete.setText("Healthy!")
+                changeImage()
+            }else
+            {
+                textGarbancete.setText("Mal hecho Healthy!")
+                clickGarbancete()
+            }
+        }
+    }
+
+    fun giroIzq(){
+        if(count != list.size) {
+            var aux = list.get(count)
+            NumberScore.setText("GiroIzq " + count)
+
+            if(aux == hamburguesa || aux == pizza){
+                textGarbancete.setText("UNNNNHealthy!")
+                changeImage()
+            }
+            else
+            {
+                textGarbancete.setText("Mal hecho UNNNNHealthy!")
+                clickGarbancete()
+            }
+        }
+    }
+
+    fun changeImage(){
+
+        var siguenteImage = list.get(count)
+        NumberScore.setText("Change " + count)
+
+        if(siguenteImage == hamburguesa){
+            HealthyText.setText(R.string.hamburguesa)
+        }
+        if(siguenteImage == pizza){
+            HealthyText.setText(R.string.pizza)
+        }
+        if(siguenteImage == lechuga){
+            HealthyText.setText(R.string.lechuga)
+        }
+        if(siguenteImage == manzana){
+            HealthyText.setText(R.string.manzana)
+        }
+        if(siguenteImage == zanahoria){
+            HealthyText.setText(R.string.zanahoria)
+        }
+        fotocentro.setImageResource(siguenteImage)
+        count++
+
+        clickGarbancete()
+    }
+
+    fun start() {
+        sensorManager.registerListener(
+            sensorEventListener,
+            sensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    fun clickGarbancete(){
+        garbancete.setOnClickListener(){
+            start()
+        }
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(sensorEventListener)
+    }
+
+    override fun onPause() {
+        stop()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        start()
+        super.onResume()
     }
 
     fun aplicarTipoDaltonismo(dalt: String) {
@@ -108,3 +238,4 @@ class HealthyUnhealthy : AppCompatActivity() {
     }
 
 }
+
